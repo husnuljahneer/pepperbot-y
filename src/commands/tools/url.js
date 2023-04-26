47cfc53ef36d69bc09384c5d391099219677cd7e
husnul <husnuljahneer19@gmail.com>
2023-04-27 03:53:42 +0530

    Debugging chatgpt

--- src/commands/tools/url.js
+++ src/commands/tools/url.js
@@ -1,29 +1,33 @@
-const Discord = require('discord.js');
-const isgd = require('isgd');
+const Discord = require("discord.js");
+const isgd = require("isgd");
 
 module.exports = async (client, interaction, args) => {
+  const url = interaction.options.getString("site");
+  const code = interaction.options.getString("code");
 
-    const url = interaction.options.getString('site');
-    const code = interaction.options.getString('code');
+  isgd.custom(url, code, function (res) {
+    if (res.startsWith("Error"))
+      return client.errNormal(
+        {
+          error: `${res.replace("Error: ", "")}`,
+          type: "editreply"
+        },
+        interaction
+      );
 
-    isgd.custom(url, code, function(res) {
-        if (res.startsWith("Error")) return client.errNormal({
-            error: `${res.replace("Error: ", "")}`,
-            type: 'editreply'
-        }, interaction)
-
-        client.succNormal({
-            text: `Your shortened url has been created!`,
-            fields: [
-                {
-                    name: `🔗┇Link`,
-                    value: `${res}`,
-                    inline: true,
-                }
-            ], 
-            type: 'editreply'
-        }, interaction);
-    });
-}
-
- 
+    client.succNormal(
+      {
+        text: `Your shortened url has been created!`,
+        fields: [
+          {
+            name: `🔗┇Link`,
+            value: `${res}`,
+            inline: true
+          }
+        ],
+        type: "editreply"
+      },
+      interaction
+    );
+  });
+};
