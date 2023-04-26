0a5468e496f740477a5ade5ab4a131edd1037ba9
husnul <husnuljahneer19@gmail.com>
2023-04-27 02:57:22 +0530

    Added ChatGpt

---
+++ src/commands/tools/chat.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const Discord = require("discord.js");
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: "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"
+});
+const openai = new OpenAIApi(configuration);
+
+module.exports = async (client, interaction, args) => {
+  const text = interaction.options.getString("text");
+
+  let prompt = `You: ${text}\n`;
+
+  const gptResponse = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: prompt,
+    max_tokens: 60,
+    temperature: 0.3,
+    top_p: 0.3,
+    presence_penalty: 0,
+    frequency_penalty: 0.5
+  });
+
+  client.embed(
+    {
+      desc: gptResponse,
+      type: "editreply"
+    },
+    interaction
+  );
+};
